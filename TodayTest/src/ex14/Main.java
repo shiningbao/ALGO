c7e7ea49d06e197a959c66e90783b69fb672eced
shiningbao <kias3036@gmail.com>
2024-02-23 01:41:29 +0900

    다시 커밋

--- TodayTest/src/ex14/Main.java
+++ TodayTest/src/ex14/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 
 // 순서 바꾸기
-/* 정수 리스트 num_list와 정수 n이 주어질 때 
+/* 정수 리스트 num_list와 정수 n이 주어질 때,
 	num_list를 n 번째 원소 이후의 원소들과 n 번째까지의 원소들로 나눠 n 번째 원소 이후의 원소들을 n 번째까지의 원소들 앞에 붙인 리스트를 return하도록 solution 함수를 완성해주세요.
 */
 public class Main {
