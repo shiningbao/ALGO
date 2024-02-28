94b9a8090ef3fbf48096ddcb2946de53edccba0e
shiningbao <kias3036@gmail.com>
2024-02-28 23:51:48 +0900

    240228 프로그래머스

--- Today/src/ex13/Main.java
+++ Today/src/ex13/Main.java
@@ -1,50 +1,51 @@
 package ex13;
 
-/*	부분 문자열 이어 붙여 문자열 만들기
-	길이가 같은 문자열 배열 my_strings와 이차원 정수 배열 parts가 매개변수로 주어집니다. 
-	parts[i]는 [s, e] 형태로, my_string[i]의 인덱스 s부터 인덱스 e까지의 부분 문자열을 의미합니다. 
-	각 my_strings의 원소의 parts에 해당하는 부분 문자열을 순서대로 이어 붙인 문자열을 return 하는 solution 함수를 작성해 주세요.
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/*	x 사이의 갯수
+  	문자열 myString이 주어집니다. 
+  	myString을 문자 "x"를 기준으로 나눴을 때 나눠진 문자열 각각의 길이를 순서대로 저장한 배열을 return 하는 solution 함수를 완성해 주세요.
 */
 public class Main {
 
 	public static void main(String[] args) {
-		String result = "";
+		int[] result = {};
 		Solution sol = new Solution();
-		result = sol.solution(new String[]{"progressive", "hamburger", "hammer", "ahocorasick"}, new int[][]{{0,4},{1,2},{3,5},{7,7}});
-		System.out.println("programmers=="+result); 		
+		result = sol.solution("oxooxoxxox");
+		System.out.println("[1, 2, 1, 0, 1, 0]=="+Arrays.toString(result)); 		
 	}
 
 }
 
 class Solution {
-    public String solution(String[] my_strings, int[][] parts) {
-		// 0.05ms~0.53ms
-		 StringBuilder answer = new StringBuilder(); // StringBuilder 객체 생성
-		 
-		 for (int i = 0; i < parts.length; i++) { 
-			 int start = parts[i][0]; 
-			 int end = parts[i][1];
-		
-			 for (int j = start; j <= end; j++) { 
-				 answer.append(my_strings[i].charAt(j));
-			 } 
-		 }
-		 
-		 return answer.toString(); // StringBuilder를 String으로 변환 -> StringBuilder 를 이용하는게 더 빠르다.
-		     
-		/* 10.23ms~38.09ms
-		 * String answer = "";
-		 * 
-		 * for (int i = 0; i < parts.length; i++) { 
-		 * 		int start = parts[i][0]; 
-		 * 		int end = parts[i][1];
-		 * 
-		 * 		for (int j = start; j <= end; j++) { 
-		 * 		answer += my_strings[i].charAt(j); 
-		 * 		} 
-		 * }
-		 * 
-		 * return answer;
-		 */
-    	}
+    public int[] solution(String myString) {
+    	ArrayList<Integer> lengths = new ArrayList<>();
+        int count = 0;
+        
+        // 이전 "x"의 위치를 저장
+        int prev= -1;
+
+        for (int i = 0; i < myString.length(); i++) {
+            if (myString.charAt(i) == 'x') {
+                // 현재 "x"의 위치에서 이전 "x"의 위치를 빼서 부분문자열의 길이를 계산하고 저장
+                lengths.add(i - prev - 1);
+                // 이전 "x"의 위치 갱신
+                prev = i;
+                count++;
+            }
+        }
+
+        // 마지막 "x" 이후에 남은 부분의 길이 저장
+        if (count > 0) {
+            lengths.add(myString.length() - prev - 1);
+        }
+
+        // ArrayList를 int 배열로 변환하여 반환
+        int[] result = new int[lengths.size()];
+        for (int i = 0; i < lengths.size(); i++) {
+            result[i] = lengths.get(i);
+        }
+        return result;
+    }
 }
