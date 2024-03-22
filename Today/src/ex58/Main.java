530ac77ba309c5fca72e4b5e9933fe280041e786
shiningbao <kias3036@gmail.com>
2024-03-23 00:30:46 +0900

    240323 프로그래머스

---
+++ Today/src/ex58/Main.java
@@ -0,0 +1,36 @@
+package ex58;
+
+import java.util.Arrays;
+
+/* 왼쪽 오른쪽
+	문자열 리스트 str_list에는 "u", "d", "l", "r" 네 개의 문자열이 여러 개 저장되어 있습니다. 
+	str_list에서 "l"과 "r" 중 먼저 나오는 문자열이 "l"이라면 해당 문자열을 기준으로 왼쪽에 있는 문자열들을 순서대로 담은 리스트를, 먼저 나오는 문자열이 "r"이라면 
+	해당 문자열을 기준으로 오른쪽에 있는 문자열들을 순서대로 담은 리스트를 return하도록 solution 함수를 완성해주세요. "l"이나 "r"이 없다면 빈 리스트를 return합니다.
+*/
+public class Main {
+
+	public static void main(String[] args) {
+		String[] result = {};
+		Solution sol = new Solution();
+		
+		result = sol.solution(new String[]{"u", "u", "l", "r"}); 
+		System.out.println("[\"u\",\"u\"]=="+Arrays.toString(result)); 
+		
+		result = sol.solution(new String[]{"l"}); 
+		System.out.println("[]=="+Arrays.toString(result)); 
+	}
+}
+
+class Solution {
+    public String[] solution(String[] str_list) { // 0.02ms ~ 0.05ms
+    	  String[] answer = {};
+          for (int i = 0; i < str_list.length;i++) {
+              if (str_list[i].equals("l")) {
+                  return Arrays.copyOfRange(str_list, 0, i);
+              } else if (str_list[i].equals("r")) {
+                  return Arrays.copyOfRange(str_list, i + 1, str_list.length);
+              }
+          }
+          return answer;
+     }
+ }
