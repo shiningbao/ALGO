2041f05240a5591d24c29701fa7d83054696de04
shiningbao <kias3036@gmail.com>
2024-04-21 23:44:36 +0900

    240421 프로그래머스

--- Introduction/src/ex30/Main.java
+++ Introduction/src/ex30/Main.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import com.sun.org.apache.xpath.internal.operations.String;
-
 /*	잘라서 배열로 저장하기
 	문자열 my_str과 n이 매개변수로 주어질 때, my_str을 길이 n씩 잘라서 저장한 배열을 return하도록 solution 함수를 완성해주세요.
 */
