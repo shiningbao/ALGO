45fc384b9a8379aa94ab615c8b5eacad2d387ca6
shiningbao <kias3036@gmail.com>
2024-05-02 15:50:20 +0900

    240502 프로그래머스

---
+++ Introduction/src/ex43/Main.java
@@ -0,0 +1,31 @@
+package ex43;
+
+/*	치킨 쿠폰
+	프로그래머스 치킨은 치킨을 시켜먹으면 한 마리당 쿠폰을 한 장 발급합니다. 쿠폰을 열 장 모으면 치킨을 한 마리 서비스로 받을 수 있고, 서비스 치킨에도 쿠폰이 발급됩니다. 
+	시켜먹은 치킨의 수 chicken이 매개변수로 주어질 때 받을 수 있는 최대 서비스 치킨의 수를 return하도록 solution 함수를 완성해주세요.
+*/
+public class Main {
+
+	public static void main(String[] args) {
+		int result = -1;
+		Solution sol = new Solution();
+		
+		result = sol.solution(100); 
+		System.out.println("11=="+result); 
+		
+		result = sol.solution(1081); 
+		System.out.println("120=="+result); 
+	}
+}
+
+class Solution {
+    public int solution(int chicken) { // 0.02ms ~ 0.04ms
+        int answer = 0;
+        
+        while(chicken>=10) {
+        	answer += chicken/10;
+        	chicken = (chicken/10) + (chicken%10);
+        }
+        return answer;
+    }
+}
